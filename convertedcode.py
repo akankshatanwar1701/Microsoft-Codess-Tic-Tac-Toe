6a1b03388eee1ed84f6d76a681da4474fe77ffee
akankshatanwar1701 <akankshatanwar73@gmail.com>
2020-07-06 15:07:32 +0530

    Changing BestMove

--- convertedcode.py
+++ convertedcode.py
@@ -1,20 +1,15 @@
-
-import numpy as np
-
-
-player = 'x'
-opponent = 'circle'
-
 def onetotwod(arr):
-	x=np.array(arr)
-	board = np.reshape(x, (-1, 3))
-	return board
 
+board=[[0 for x in range(3)] for y in range(3)]
+	k=0
+	for i in range(3):
+		for j in range(3):
+			board[i][j]=arr[k++]
+return board
 
-def twotooned(board):
-	arr=board.flatten()
-	return arr
-
+def twotooned(row,col):
+       index= col+3*row
+       return index
 
 def isMovesLeft(board):
 	for x in range(3):
@@ -23,17 +18,16 @@ def isMovesLeft(board):
 				return True
 	return False
 
-
-def evaluate(b):  
+def evaluate(b, player, opponent):  
    
     # Checking for Rows for X or O victory.  
     for row in range(0, 3):  
        
         if b[row][0] == b[row][1] and b[row][1] == b[row][2]:  
            
-            if b[row][0] == 'x': 
+            if b[row][0] == player: 
                 return 10 
-            elif b[row][0] == 'circle':  
+            elif b[row][0] == opponent:  
                 return -10 
   
     # Checking for Columns for X or O victory.  
@@ -41,24 +35,24 @@ def evaluate(b):
        
         if b[0][col] == b[1][col] and b[1][col] == b[2][col]:  
            
-            if b[0][col]=='x': 
+            if b[0][col]==player: 
                 return 10 
-            elif b[0][col] == 'circle':  
+            elif b[0][col] == opponent:  
                 return -10 
   
     # Checking for Diagonals for X or O victory.  
     if b[0][0] == b[1][1] and b[1][1] == b[2][2]:  
        
-        if b[0][0] == 'x':  
+        if b[0][0] == player:  
             return 10 
-        elif b[0][0] == 'circle':  
+        elif b[0][0] == opponent:  
             return -10 
        
     if b[0][2] == b[1][1] and b[1][1] == b[2][0]:  
        
-        if b[0][2] == 'x':  
+        if b[0][2] == player:  
             return 10 
-        elif b[0][2] == 'circle':  
+        elif b[0][2] == opponent:  
             return -10 
        
     # Else if none of them have won then return 0  
@@ -66,9 +60,11 @@ def evaluate(b):
 
 
 
-def minimax(board, depth, isMax):
 
-	score = evaluate(board)
+
+def minimax(board, depth, isMax, player, opponent):
+
+	score = evaluate(board,player,opponent)
 	#if maximiser won the game return it's score
 
 	if score == 10:
@@ -94,7 +90,7 @@ def minimax(board, depth, isMax):
 			for y in range(3):
 				if board[x][y] = 'blank':
 					board[x][y] = player
-					best = max( best, minimax(board, depth+1, !isMax) )
+					best = max( best, minimax(board, depth+1, !isMax,player,opponent) )
 					board[x][y] = 'blank'
 
 		return best
@@ -106,14 +102,17 @@ def minimax(board, depth, isMax):
 			for y in range(3):
 				if board[x][y] == 'blank':
 					board[x][y] = opponent
-					best = min( best,  minimax(board, depth+1, !isMax) )
+					best = min( best,  minimax(board, depth+1, !isMax,player,opponent) )
 					board[x][y] = 'blank'
 
 		return best
 
 
-def findBestMove(board):
-	bestVal = -1000
+def findBestMove(arr, player, opponent):
+
+	board= onetotwod(arr)
+	
+bestVal = -1000
 	row = -1
 	col = -1
 
@@ -121,7 +120,7 @@ def findBestMove(board):
 		for y in range(3):
 			if board[x][y] == 'blank':
 				board[x][y] = player
-				moveVal = minimax(board, 0, false)
+				moveVal = minimax(board, 0, false, player, opponent)
 				board[x][y] = 'blank'
 
 				if moveVal > bestVal:
@@ -129,9 +128,7 @@ def findBestMove(board):
 					col = y
 					bestVal = moveVal
 
-	return row, col
-
-
+	return twotooned(row,col)
 
 
 
